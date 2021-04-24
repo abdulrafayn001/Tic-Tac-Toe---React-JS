c03807ca42fba1ad4853a3cc4f861f60a57bac78
Abdulrafay <abdulrafayn001@gmail.com>
2021-04-24 14:55:07 -0700

    Remove showBorad state and conditional statement on showBoard from Board.js

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,8 +7,7 @@ class Board extends Component {
 
         this.state={
             squares:Array(9).fill(null),
-            xTurn:true,
-            showBorad:true
+            xTurn:true
         }
     }
 
@@ -50,29 +49,23 @@ class Board extends Component {
         return (
             <div>
                 <div className={Classes.Status}>{status}</div>
-                {this.state.showBorad?
-                    <div>
-                        <div className="board-row">
-                            {this.renderSquare(0)}
-                            {this.renderSquare(1)}
-                            {this.renderSquare(2)}
-                        </div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
 
-                        <div className="board-row">
-                            {this.renderSquare(3)}
-                            {this.renderSquare(4)}
-                            {this.renderSquare(5)}
-                        </div> 
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div> 
 
-                        <div className="board-row">
-                            {this.renderSquare(6)}
-                            {this.renderSquare(7)}
-                            {this.renderSquare(8)}
-                        </div>
-                    </div>
-                    :
-                    <br/>
-                }               
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>              
             </div>
         )
     }
