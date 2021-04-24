53f354d88f120406a63c247c1864b092dd7367db
Abdulrafay <abdulrafayn001@gmail.com>
2021-04-24 15:20:44 -0700

    Reset Button added without functionality

--- src/components/Board.js
+++ src/components/Board.js
@@ -49,23 +49,28 @@ class Board extends Component {
         return (
             <div>
                 <div className={Classes.Status}>{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+                <div>
+                    <div className="board-row">
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                    </div>
+
+                    <div className="board-row">
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                    </div> 
+
+                    <div className="board-row">
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                    </div>
                 </div>
-
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div> 
-
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>              
+                <div class={Classes.ResetBtn}>
+                    <button class="button">RESET</button>
+                </div>
             </div>
         )
     }
